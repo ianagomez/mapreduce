3b53800698348dca698b9f9baef79fde01e2030c
ianagomez <106987538+ianagomez@users.noreply.github.com>
2022-10-11 13:37:50 +0200

    Modificacion  del error xcvxcvxcvx
    Hemos modificado 2 ficheros:tradme.md y friendrecommenderjob.java

--- src/main/java/com/agartime/utad/friendrecommender/friendrecommenderjob.java
+++ src/main/java/com/agartime/utad/friendrecommender/friendrecommenderjob.java
@@ -1,6 +1,7 @@
+
+
 package com.agartime.utad.friendrecommender;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
